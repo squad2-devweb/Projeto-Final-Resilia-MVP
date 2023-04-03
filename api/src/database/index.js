4a33b9bd8fc1e9a14bf05ae614f292662a6ac1eb
LzLuscas <115434062+LzLuscas@users.noreply.github.com>
2023-04-03 15:58:23 -0300

    Delete api directory

--- api/src/database/index.js
+++
@@ -1,9 +0,0 @@
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
-
-export async function Database () {
-    return open({
-      filename: './database.db',
-      driver: sqlite3.Database
-    })
-  }
